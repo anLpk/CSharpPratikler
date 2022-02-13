d0acca8e0d857006025912f31e3d5403d6e18066
anlpk <anilpak35@gmail.com>
2022-02-13 15:29:17 +1100

    switch case ornekleri tamamlandi

--- KararYapilari/Program.cs
+++ KararYapilari/Program.cs
@@ -21,5 +21,65 @@ namespace KararYapilari
 
             Console.WriteLine(sonuc);
         }
+
+        static void SwitchCaseCondition(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayindasiniz!");
+                break;
+                
+                case 2:
+                    Console.WriteLine("Subat Ayindasiniz!");
+                break;
+                
+                case 3:
+                    Console.WriteLine("Mart Ayindasiniz!");
+                break;
+                
+                case 4:
+                    Console.WriteLine("Nisan Ayindasiniz!");
+                break;
+                                
+                default:
+                    Console.WriteLine("Yanlis veri girdiniz!");
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kis Ayindasiniz!");
+                break;
+                
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Ilkbahar Ayindasiniz!");
+                break;
+                
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Ayindasiniz!");
+                break;
+                
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar Ayindasiniz!");
+                break;
+                                
+                default:
+                    Console.WriteLine("Yanlis veri girdiniz!");
+                break;
+            }
+        }
     }
 }
