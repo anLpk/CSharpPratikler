200b3dcdc1d829bcfaccb4173f202e442e7abccc
anlpk <anilpak35@gmail.com>
2022-02-13 16:32:52 +1100

    while ve foreach ornekleri tamamlandi

---
+++ DongulerWhileForEach/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DongulerWhileForEach
+{
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lutfen bir sayı giriniz : ");
+            int sayi1 = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac <= sayi1)
+            {
+                toplam += sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam / sayi);
+
+
+            char character = 'a';
+            while (character < 'z')
+            {
+                Console.WriteLine(character);
+                character++;
+            }
+
+            System.Console.WriteLine("*** Foreach ***");
+            string[] arabalar = { "BMW", "Ford", "Toyota", "Nissan" };
+
+            foreach (var araba in arabalar)
+            {
+               Console.WriteLine(araba);
+            }
+        }
+    }
+}
