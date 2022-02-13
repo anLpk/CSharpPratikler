a67123b963c446150f8e7604224405418133ed09
anlpk <anilpak35@gmail.com>
2022-02-13 12:09:33 +1100

    c# 101 dersi kapsamindaki ilk repo

---
+++ ConsoleProgramlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleProgramlama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Isminizi giriniz:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadinizi giriniz:");
+            string surname = Console.ReadLine();
+            Console.WriteLine($"Merhaba {name} {surname}");
+        }
+    }
+}
